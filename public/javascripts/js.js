d34fe2c16ede1b0ba2e7fa194123aba898199e33
shiyunlong123 <836738627@qq.com>
2017-10-24 09:11:48 +0800

    10.23

---
+++ public/javascripts/js.js
@@ -0,0 +1,8 @@
+window.onload= function () {
+    li.onmouseover = function () {
+        ol.style.display = 'block';
+    };
+    li.onmouseout = function () {
+        ol.style.display = 'none';
+    };
+};
